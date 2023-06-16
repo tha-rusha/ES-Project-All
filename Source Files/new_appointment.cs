66a0806eb1aab77d309188dc2c04c81849edecdc
Tharusha Dilhara <86361836+tha-rusha@users.noreply.github.com>
2023-06-17 01:20:33 +0530

    Add files via upload
    Source Files of the CRM System

---
+++ Source Files/new_appointment.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace es_all
+{
+    public partial class new_appointment : Form
+    {
+        public string conString = "Server=localhost\\SQLEXPRESS;Database=escrm;Trusted_Connection=True";
+        public SqlConnection con;
+        public new_appointment()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void new_appointment_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(conString);
+            con.Open();
+            string loadcustomerQuery = "SELECT firstName FROM customer";
+            SqlCommand customorCommand = new SqlCommand(loadcustomerQuery, con);
+            SqlDataReader customerDataReader = customorCommand.ExecuteReader();
+            comboBox1.Items.Clear();
+            while (customerDataReader.Read())
+            {
+                comboBox1.Items.Add(customerDataReader["firstName"].ToString());
+            }
+            customerDataReader.Close();
+
+            //Select id
+            SqlCommand loadId = new SqlCommand("SELECT id FROM appointment", con);
+            SqlDataReader idDataReader = loadId.ExecuteReader();
+
+            while (idDataReader.Read())
+            {
+                comboBox3.Items.Add(idDataReader["id"]);
+            }
+            idDataReader.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String referenceNumber = textBox3.Text;
+            String venue = textBox1.Text;
+            DateTime dateTime = dateTimePicker1.Value.Date;
+            String person = comboBox1.Text;
+            String topic = textBox4.Text;
+            SqlCommand saveAppointment = new SqlCommand($"INSERT INTO appointment(referanceNumber,venue,dateTime,person,topic)VALUES('{referenceNumber}','{venue}','{dateTime}','{person}','{topic}')", con);
+            saveAppointment.ExecuteNonQuery();
+            textBox3.Clear();
+            textBox1.Clear();
+            textBox4.Clear();
+
+            //comment
+
+            MessageBox.Show("New Appointmet added");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            textBox1.Clear();
+            textBox4.Clear();
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //load data when selected id is change in customer
+
+            int selectedId = int.Parse(comboBox3.Text);
+            SqlCommand loadAppointment = new SqlCommand($"SELECT * FROM appointment WHERE id = {selectedId}", con);
+            SqlDataReader selectedAppointment = loadAppointment.ExecuteReader();
+
+            while (selectedAppointment.Read())
+            {
+                textBox3.Text = selectedAppointment[1].ToString();
+                textBox1.Text = selectedAppointment[2].ToString();
+                dateTimePicker1.Text = selectedAppointment[3].ToString();
+                comboBox1.Text = selectedAppointment[4].ToString();
+                textBox4.Text = selectedAppointment[5].ToString();  
+            }
+            selectedAppointment.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int selectedId = int.Parse(comboBox3.Text);
+            SqlCommand delete = new SqlCommand($"DELETE FROM appointment WHERE id='{selectedId}'", con);
+
+            DialogResult dialogResult = MessageBox.Show(
+                "Are you sure want to delete?", "Delete", MessageBoxButtons.YesNo
+                );
+            if (dialogResult == DialogResult.Yes)
+            {
+                SqlDataReader reader = delete.ExecuteReader();
+                reader.Close();
+
+                MessageBox.Show("Record deleted!");
+
+                this.Close();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string refNum = textBox3.Text;
+            string venue = textBox1.Text;
+            DateTime dateTime = dateTimePicker1.Value.Date;
+            string person = comboBox1.Text;
+            string topic = textBox4.Text;
+
+            int selectedId = int.Parse(comboBox3.Text);
+
+            SqlCommand update = new SqlCommand($"UPDATE appointment SET referanceNumber = '{refNum}', venue= '{venue}',dateTime='{dateTime}',person='{person}',topic='{topic}' WHERE id = '{selectedId}'", con);
+            update.ExecuteNonQuery();
+
+            MessageBox.Show("Record update successully!");
+        }
+    }
+}
---
+++ Source Files/new_opportunity.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace es_all
+{
+    public partial class new_opportunity : Form
+    {
+        public string conString = "Server=localhost\\SQLEXPRESS;Database=escrm;Trusted_Connection=True";
+        public SqlConnection con;
+        public new_opportunity()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String referenceNumber = textBox5.Text;
+            String company = comboBox1.Text;
+            String person = comboBox2.Text;
+            String probability = textBox3.Text;
+            String description = textBox4.Text;
+            SqlCommand saveopportunity = new SqlCommand($"INSERT INTO opportunity(referanceNumber,company,person,probability,description)VALUES('{referenceNumber}','{company}','{person}','{probability}','{description}')", con);
+            saveopportunity.ExecuteNonQuery();
+            textBox5.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+            MessageBox.Show("New opportunity added");
+        }
+
+        private void new_opportunity_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(conString);
+            con.Open();
+            string loadcompanyQuery = "SELECT companyName FROM company";
+            SqlCommand companyCommand = new SqlCommand(loadcompanyQuery, con);
+            SqlDataReader companyDataReader = companyCommand.ExecuteReader();
+            comboBox1.Items.Clear();
+            while (companyDataReader.Read())
+            {
+                comboBox1.Items.Add(companyDataReader["companyName"].ToString());
+            }
+            companyDataReader.Close();
+
+            string loadcustomerQuery = "SELECT * FROM customer";
+            SqlCommand customerCommand = new SqlCommand(loadcustomerQuery, con);
+            SqlDataReader customerDataReader = customerCommand.ExecuteReader();
+            comboBox2.Items.Clear();
+            while (customerDataReader.Read())
+            {
+                comboBox2.Items.Add(customerDataReader["firstName"].ToString()+" "+customerDataReader["lastName"].ToString());
+            }
+            customerDataReader.Close();
+
+            //Select id
+            SqlCommand loadId = new SqlCommand("SELECT id FROM opportunity", con);
+            SqlDataReader idDataReader = loadId.ExecuteReader();
+
+            while (idDataReader.Read())
+            {
+                comboBox3.Items.Add(idDataReader["id"]);
+            }
+            idDataReader.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox5.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //load data when selected id is change in Opportunity
+
+            int selectedId = int.Parse(comboBox3.Text);
+            SqlCommand loadOpportunity = new SqlCommand($"SELECT * FROM opportunity WHERE id = {selectedId}", con);
+            SqlDataReader selectedOpportunity = loadOpportunity.ExecuteReader();
+
+            while (selectedOpportunity.Read())
+            {
+                textBox5.Text = selectedOpportunity[1].ToString();
+                comboBox1.Text = selectedOpportunity[2].ToString();
+                comboBox2.Text = selectedOpportunity[3].ToString();
+                textBox3.Text = selectedOpportunity[4].ToString();
+                textBox4.Text = selectedOpportunity[5].ToString();
+            }
+            selectedOpportunity.Close();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string refNum = textBox5.Text;
+            string company = comboBox1.Text;
+            string person = comboBox2.Text;
+            int prob = int.Parse(textBox3.Text);
+            string desc = textBox4.Text;
+
+
+
+            int selectedId = int.Parse(comboBox3.Text);
+
+            SqlCommand update = new SqlCommand($"UPDATE opportunity SET [referanceNumber] = '{refNum}', company= '{company}',person='{person}',probability='{prob}',description='{desc}' WHERE id = '{selectedId}'", con);
+            update.ExecuteNonQuery();
+
+            MessageBox.Show("Record update successully!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int selectedId = int.Parse(comboBox3.Text);
+            SqlCommand delete = new SqlCommand($"DELETE FROM opportunity WHERE id='{selectedId}'", con);
+
+            DialogResult dialogResult = MessageBox.Show(
+                "Are you sure want to delete?", "Delete", MessageBoxButtons.YesNo
+                );
+            if (dialogResult == DialogResult.Yes)
+            {
+                SqlDataReader reader = delete.ExecuteReader();
+                reader.Close();
+
+                MessageBox.Show("Record deleted!");
+
+                this.Close();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
